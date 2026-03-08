045e7631a5e9c955cfdfaf70ee72ad865f9855fb
Kristijana Marušić <kristijanam@gen-i.si>
2026-03-08 16:58:37 +0100

    Chapter 14. Change

--- src/main/java/Bank.java
+++ src/main/java/Bank.java
@@ -1,12 +1,26 @@
+import java.util.Hashtable;
+
 public class Bank {
 
     Money reduce (Expression source, String to) {
         if (source instanceof Money) 
-            return (Money) source.reduce(to);
+            return (Money) source.reduce(this, to);
         Sum sum = (Sum) source;
         //int amount = sum.augend.amount + sum.addend.amount;
         //return Money.dollar(10);
         //return new Money(amount, to);
-        return sum.reduce(to);
+        return source.reduce(this, to);
+    }
+
+    int rate (String from, String to) {
+        if (from.equals(to)) return 1;
+        Integer rate = (Integer) rates.get(new Pair(from, to));
+        return rate.intValue();
+    }
+
+    private Hashtable rates = new Hashtable();
+
+    void addRate(String from, String to, int rate) {
+        rates.put(new Pair(from, to), new Integer(rate));
     }
 }
--- src/test/java/MoneyTest.java
+++ src/test/java/MoneyTest.java
@@ -78,4 +78,22 @@ public class MoneyTest {
         Money result = bank.reduce(Money.dollar(1), "USD");
         assertEquals(Money.dollar(1), result);
     }
+
+    @Test
+    void testReduceMoneyDifferentCurrency() {
+        Bank bank = new Bank();
+        bank.addRate("CHF", "USD", 2);
+        Money result = bank.reduce(Money.franc(2), "USD");
+        assertEquals(Money.dollar(1), result);
+    }
+
+    @Test
+    void testArrayEquals() {
+        assertArrayEquals(new Object[]{"abc"}, new Object[]{"abc"});
+    }
+
+    @Test
+    void testIdentityRate() {
+        assertEquals(1, new Bank().rate("USD", "USD"));
+    }
 }
